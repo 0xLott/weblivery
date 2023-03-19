b78091b367b6250b223418b4141edf3783ae069b
Yan Nalon <havercraft1@gmail.com>
2023-03-19 13:30:40 -0300

    Schemas criados

--- Codigo/app.js
+++ Codigo/app.js
@@ -16,18 +16,55 @@ app.use(session({secret: 'Weblivery', resave: false, saveUninitialized: true }))
 app.use(passport.initialize())
 app.use(passport.session())
 
-//mongoose.connect('mongodb+srv://' + process.env.DB_USER + ':' + process.env.DB_PASS + '@db-cluster.cjjdosp.mongodb.net/<BD_NAME>')
+mongoose.connect('mongodb+srv://' + process.env.DB_USER + ':' + process.env.DB_PASS + '@db-cluster.cjjdosp.mongodb.net/weblivery')
 
-// Schemas
+// Conferir se os schemas batem certo com o gp
 
-// Passport Serialization
+const todoItemSchema = new mongoose.Schema({
+    content: String,
+    project: String,
+})
 
-// Test Route
+const projectSchema = new mongoose.Schema({
+    name: String,
+    description: String,
+    client: String,
+    status: String,
+})
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String,
+    name: String,
+    role: String,
+    projects: [projectSchema],
+    todolist: [todoItemSchema]
+})
+
+userSchema.plugin(passportLocalMongoose, {usernameField: 'email'})
+
+const User = new mongoose.model("User", userSchema)
+
+const Project = new mongoose.model("Project", projectSchema)
+
+const ToDoItem = new mongoose.model("ToDoItem", todoItemSchema)
+
+passport.use(User.createStrategy())
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
+
+// ROTAS DE TESTE
 
 app.get('/', (req, res) => {
     res.render('form')
 })
 
+app.post('/', (req, res) => {
+    User.register({email: req.body.email}, req.body.password, (err, newUser) => {
+        // Ta criando o user no Mongo certinho
+    })
+})
+
 app.listen(3000)
 
-// app.listen(process.env.PORT || 3000)
+// app.listen(process.env.PORT) para quando for dar deploy
